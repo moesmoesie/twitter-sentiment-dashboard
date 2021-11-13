70dfc856e2bbb5b386ba7fa2d5c7704fb37fcbfe
Mustafa <mndarwesh@protonmail.com>
2021-11-13 17:33:40 +0100

    Make metrics responsive

--- src/app/components/metrics-container/metrics-container.component.ts
+++ src/app/components/metrics-container/metrics-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/services/global/global.service';
 
 @Component({
   selector: 'metrics-container',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetricsContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(public global : GlobalService) { }
 
   ngOnInit(): void {
   }
