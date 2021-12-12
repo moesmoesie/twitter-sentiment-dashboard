4831d320de2f936e014535c6fc5709f9e18fb415
Mustafa <mndarwesh@protonmail.com>
2021-12-12 18:07:37 +0100

    Remove text from keyword section

--- src/app/components/keyword-section/keyword-section.component.ts
+++ src/app/components/keyword-section/keyword-section.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keyword-section.component.scss']
 })
 export class KeywordSectionComponent implements OnInit {
-  content = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates utamet, magni nihil totam beatae autem asperiores."
-  title = "Keywords"
   constructor() { }
 
   ngOnInit(): void {
