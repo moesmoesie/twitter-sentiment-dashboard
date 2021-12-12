dfdc66085d7182f46f4fad7c67d9c8691e957e80
Mustafa <mndarwesh@protonmail.com>
2021-12-12 23:13:32 +0100

    Change budgets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.{html,ts,css}',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   important: true,
+  jit: true,
   theme: {
     extend: {
       colors: {
